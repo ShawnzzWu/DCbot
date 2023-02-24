b7ca7de119318c56f9167b05f9bd5579fd32afe5
dzwu <dezhang.wu@gmail.com>
2023-02-24 10:19:34 -0500

    version 1

---
+++ bot.py
@@ -0,0 +1,173 @@
+# bot.py
+import os
+import ssl
+import discord
+
+import chat_exporter
+# import pandas as pd
+# import random
+from dotenv import load_dotenv
+from botcds import *
+
+
+
+#############################
+#Replying Section
+#############################
+
+intents = discord.Intents.all()
+
+load_dotenv("D:/python/dc/inventory/dc.env")
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')  
+BOT_CHANNEL = int(os.getenv(('DISCORD_BOTCHANNEL')))
+
+#Feedback resources
+path = 'D:\dcbot'
+
+pf = pd.read_csv(path + '\pf.csv')
+ef = pd.read_csv(path + '\ef.csv', keep_default_na=False)
+
+
+
+
+
+#Instaniate bot connection client
+client = discord.Client(intents = intents)
+
+
+
+#############################
+#Replying Section
+#############################
+
+#A function to detect if specific word is appearing in the context
+async def embedded_feedback(message):
+    for i, r in ef.iterrows():
+
+        if r['include'] in message.content:
+            if len(r['exclude']) > 0:
+                if r['exclude'] not in message.content:
+                    await message.channel.send(r['print'])
+            else:
+                await message.channel.send(r['print'])
+
+
+#A function to detect if specific word is the context
+async def feedback(message):
+    # print(message.content, pf[['include']].values, message.content in pf[['include']].values)
+    if message.content in pf[['include']].values:
+        index = pf[pf['include'] == message.content].index[0]
+        await message.channel.send(pf['print'].iloc[index])
+    else:
+        await embedded_feedback(message)
+
+
+
+
+# async def poll(message):
+
+# async def poker(message):
+
+#A function to check the command and operate commands
+async def command(message):
+        temp = message.content[1:].split()
+
+        global pf
+        global target
+
+        for i in commandlst:
+            if temp[0] == i[0]:
+                await i[3](message)
+
+        # if temp[0] == 'output':
+        #     await history_generator(message)
+        # elif temp[0] == 'pfcreate':
+        #     await pfcreator(message)
+        # elif temp[0] == 'r':
+        #     await rdpick(message)
+        # elif temp[0] == 'fk':
+        #     await fk(message)
+        # elif temp[0] == 'list':
+        #     await cdlst(message)
+
+
+
+#A function to detect command or chat to reply
+async def reading(message):
+
+    if message.content[0] != '[' or len(message.content) <= 1:
+        await feedback(message)
+    else:
+        await command(message)
+
+
+
+
+#############################
+#Event Section
+#############################
+
+#detect message event sent by non-bot user
+@client.event
+async def on_message(message):
+
+    if message.author.bot:
+        return
+
+    #Detect if the user is being targeted by a verbal abuse
+    if str(message.author.id) == list(target.keys())[0][2: -1]:
+        await attack(message)
+
+    await reading(message)
+
+
+@client.event
+async def on_ready():
+
+    # for guild in client.guilds:
+    #     if guild.name == GUILD:
+    #         break
+    # print(client.guilds) SequenceProxy(dict_values([<Guild id=890789267687764021 name='杀鸡' shard_id=0 chunked=False member_count=97>]))
+
+    #discord.utils.get(iterable, /, **attrs)
+    #A helper that returns the first element in the iterable that meets all the traits passed in attrs. This is an alternative for find().
+
+#When multiple attributes are specified, they are checked using logical AND, not logical OR. Meaning they have to meet every attribute passed in and not one of them.
+    guild = discord.utils.get(client.guilds, name = '杀鸡')
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})')
+
+
+    # channel = client.get_channel(BOT_CHANNEL)  # Gets channel from internal cache
+    #
+    # await channel.send('芜湖 起飞!')
+
+    # members = '\n - '.join([member.name for member in guild.members])
+    # print(f'Guild Members:\n - {members}')
+
+
+# @client.event
+# async def on_member_join(member):
+#     await member.create_dm()
+#     await member.dm_channel.send(
+#         f'Hi {member.name}, '
+#     )
+
+##############################
+# bot = commands.Bot(command_prefix='[', intents=intents)
+#
+# @bot.event
+# async def on_ready():
+#     print(f'{bot.user.name} has connected to Discord!')
+#
+# bot.run(TOKEN)
+
+
+#############################
+#Operating Section
+#############################
+client.run(TOKEN)
---
+++ botcds.py
@@ -0,0 +1,135 @@
+import random
+import pandas as pd
+import json
+
+
+
+#Feedback resources
+path = 'D:\dcbot'
+
+pf = pd.read_csv(path + '//pf.csv')
+ef = pd.read_csv(path + '//ef.csv', keep_default_na=False)
+
+with open(path + '//target.json', 'r') as jsfile:
+    target = json.load(jsfile)
+jsfile.close()
+
+vbabuse = ['作为一个bot， 我想用塑料小鸭打爆你的狗头', '说慢点，我狗话不是很好' , '天晴了，雨停了，你就觉得你行了。', '你这种智商看20集智慧树都补不回来',
+           '等我有钱了，我就带你去最好的神经病院。', '我真的很想帮你治疗，但我是一名家庭医生，而你是一个孤儿。', '等你死了，我直接坐飞机到你的坟头，疯狂地偷吃你的贡品',
+           '小学毕业了吗？ 一篇报纸读的下来吗？', '本大爹将你这弱智小儿一手击在墙上抠都抠不下来', '祝你把把对K碰对A', '如果让人犯恶心是刑事犯罪，你得死刑立即执行。',
+           '脑子是个好东西，希望人人都可以有', '我把你挂到迎客松上喜迎八方贵客', '我这聊着聊着还有点饿了呢 emmm想拿你骨灰熬点稀饭', '您看我像配钥匙的是吧？行，您配几把？']
+
+#############################
+#Command Section
+#############################
+
+#A function to transform a piece of message into a dataframe
+def history_df(author = '', create_at = '', content = ''):
+    return pd.DataFrame({'author': [author], 'create_at': [create_at], 'content': [content]})
+
+
+#A function to output the chat history of the channel that the command is sent
+async def history_generator(message):
+        df = pd.DataFrame({'author': [], 'create_at': [], 'content': []})
+        async for msg in message.channel.history(limit = 50):
+            if msg.author.bot:
+                continue
+
+            if len(msg.content) > 0:
+                if msg.content[0] != '[':
+                    df = pd.concat([df, history_df(msg.author, msg.created_at, msg.content)])
+
+
+        df.to_csv(path + '\dchistory.csv', encoding='utf_8_sig')
+        print('File generated successfully!')
+
+
+#A function to add or alter reply
+async def pfcreator(message):
+        global pf
+
+        temp = message.content[1:].split()
+        if len(temp) < 3:
+            await message.channel.send('command pfcreate usage: [pfcreate <content should include> <content reply>')
+
+        if temp[1] not in pf[['include']].values:
+
+            try:
+                msg = pd.DataFrame({'include': [temp[1]], 'print': [temp[2]]})
+
+                pf = pd.concat([pf, msg]).reset_index()
+                pf = pf[['include', 'print']]
+
+                pf.to_csv(path + '\pf.csv', encoding='utf_8_sig')
+                await message.channel.send('New record created')
+            except:
+                await message.channel.send('Something is wrong')
+        else:
+            pf['print'][pf['include'] == temp[1]] = temp[2]
+            pf = pf[['include', 'print']]
+            pf.to_csv(path + '\pf.csv', encoding='utf_8_sig')
+            await message.channel.send('Record altered')
+
+
+#A function to set a user as the target of verbal abuse
+async def fk(message):
+
+    # Add the user name to a dictionary, describing the user being targeted and the number of times to be attacked
+    target = {message.content[4:]: 5}
+    with open(path + '//target.json', 'w') as jsfile:
+        json.dump(target, jsfile)
+    jsfile.close()
+
+    await message.channel.send(list(target.keys())[0] + ' 我喊你一声你敢答应吗')
+
+
+#A function to verbal abuse a user
+async def attack(message):
+    global target, vbabuse
+
+    user = list(target.keys())[0]
+
+    if target[user] == 0:
+        target = {'temp' : 0}
+        return
+    else:
+        target[user] -= 1
+        await message.channel.send(list(target.keys())[0] + random.choice(vbabuse))
+
+
+#A function to generate random number in a range
+async def rdpick(message):
+    response = '幸运数字就是： '
+
+    temp = message.content[1:].split()
+    print(temp, response + str(random.choice(range(int(temp[1])))))
+    try:
+        if len(temp) == 2:
+            await message.channel.send(response + str(random.choice(range(int(temp[1])))))
+        if len(temp) == 3:
+            await message.channel.send(response + str(random.choice(range(int(temp[1]), int(temp[2])))))
+    except:
+        await message.channel.send('usage: +r <start num default 0> <end num>')
+
+
+
+
+
+#A function to list all the commands
+async def cdlst(message):
+    global commandlst
+
+    menu = ''
+
+    for i in range(len(commandlst)):
+        menu = menu + '{:<30}{:<20}{:<20}\n'.format(commandlst[i][0], commandlst[i][1], commandlst[i][2])
+
+    await message.channel.send(menu)
+
+
+
+#Help resources
+commandlst = [['fk', '语言攻击一个用户', 'usage: [fk <@some user>', fk],
+              ['r' ,  '生成一个随机数', 'usage: [r <start num default 0> <end num>', rdpick],
+              ['pfcreate', '添加一条关键词检测回复', 'usage: [pfcreate <content should include> <content reply>', pfcreator],
+              ['list' ,    '显示所有可用指令', 'usage: [list', cdlst]]
